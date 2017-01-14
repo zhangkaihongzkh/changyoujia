d035d9c48f50813ac93225808687776b4f997980
zhangkaihongzkh <953628520@qq.com>
2017-01-14 16:17:54 +0800

    增加了swiper插件 同时修改图片为透明背景

--- src/js/home.js
+++ src/js/home.js
@@ -2,34 +2,43 @@ $(function(){
 	//广告区域的轮播
 	(function(window){
 		
-		
+		var mySwiper = new Swiper ('.swiper-container', {
+	    direction: 'horizontal',
+	    loop: true,
+	    
+	    
+	    // 如果需要前进后退按钮
+	    nextButton: '.swiper-button-next',
+	    prevButton: '.swiper-button-prev',
+	    
+	  })        
 	})(window);
 
 	//侧边栏动画效果
 	(function(window){
 		//用户
 		$('#sidebar .sidebar-user').hover(function(){
-			$('.user-center').stop().animate({
-				left:'-=101px',
+			$('.user-center').animate({
+				left:'-=100px',
 				opacity:1,
 
 			},400);
 		},function(){
-			$('.user-center').stop().animate({
-				left:'+=101px',
+			$('.user-center').animate({
+				left:'+=100px',
 				opacity:0,
 			},400);
 		});
 		//客服中心联系
 		$('#sidebar .sidebar-phone').hover(function(){
 			$('.contact-phone').animate({
-				left:'-=171px',
+				left:'-=170px',
 				opacity:1,
 
 			},400);
 		},function(){
 			$('.contact-phone').animate({
-				left:'+=171px',
+				left:'+=170px',
 				opacity:0,
 			},400);
 		});
