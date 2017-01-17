1d8fa894bca3da28a1240a0bfe88281948f0f1a8
zhangkaihongzkh <953628520@qq.com>
2017-01-17 22:26:58 +0800

    积分商城首页-done

--- src/js/points-shop.js
+++ src/js/points-shop.js
@@ -1,20 +1,19 @@
 //TAB控制函数
 function tabs(tabId, tabNum){
-	//设置点击后的样式
+	/*//设置点击后的样式
 	$(tabId + " .form-tab li").removeClass('active');
-	$(tabId + " .form-tab li").eq(tabNum).addClass('active');
+	$(tabId + " .form-tab li").eq(tabNum).addClass('active');*/
 	//根据参数决定要显示的内容
 	$(tabId + " .tabcon").hide();
 	$(tabId + " .tabcon").eq(tabNum).show();
 }	
 $(function(){
-
 	(function(window){
-		$('.form-tab li').on('click',function(){
+
+		$('#points-shop-main-content .points-shop-right .points-shop-tabs li').on('click',function(){
 			var ind = $(this).index();//获取当前li的值
-			
 			$(this).addClass('active').siblings().removeClass('active');
-			tabs(".content-box",ind);
+			tabs("#points-shop-main-content .poins-shop-wrapper",ind);
 		});
 	})(window);
 });
