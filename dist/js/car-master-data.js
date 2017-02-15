ef08b8d87893e45ad26b4aa240aa851860a7364c
zhangkaihongzkh <953628520@qq.com>
2017-02-15 16:05:27 +0800

    车师傅详情页面 -done

---
+++ dist/js/car-master-data.js
@@ -0,0 +1,40 @@
+$(function(){
+	//添加标签
+	(function(){
+		var $addTagsContentWrapper = $("#car-master-add-tags-content");
+		var $tagsWrapper = $addTagsContentWrapper.find('.add-tags-wrapper');
+		var $addBtn = $addTagsContentWrapper.find(".add-tags-button");
+		$addBtn.on('click',function(){
+			layer.prompt(function(value, index, elem){
+				var $liContent = $('<li class="add-tags-item selected">'+ value +'</li>');
+				$addBtn.before($liContent);
+				layer.close(index);
+				$liContent.on('click',function(){
+					if($(this).hasClass('selected')){
+						$(this).removeClass('selected');
+					}else{
+						$(this).addClass('selected');
+					}
+				});
+			});
+		});
+	})(window);
+
+	//点击标签样式
+	(function(window){
+		//获取到点击的ul
+		var $tagsWrapper = $('#car-master-add-tags-content .add-tags-wrapper');
+		//获取到每一个li
+		var $tagsItem = $tagsWrapper.find('li');
+		$tagsItem.each(function(){
+			var $item = $(this);
+			$item.on('click',function(){
+				if($item.hasClass('selected')){
+					$item.removeClass('selected');
+				}else{
+					$item.addClass('selected');
+				}
+			});
+		});
+	})(window);
+});
